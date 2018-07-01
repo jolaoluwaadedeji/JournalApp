5bd8b1fc05c75fa6fafae270a5ce84a47f45fb56
Adedeji Jola <adedejijolaoluwa@gmail.com>
2018-07-01 23:53:09 +0100

    Fixed RegistrationActivity

--- app/src/main/java/com/example/jola/journalapp/RegistrationActivity.java
+++ app/src/main/java/com/example/jola/journalapp/RegistrationActivity.java
@@ -1,16 +1,20 @@
 package com.example.jola.journalapp;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity{
 
     EditText email;
     EditText password;
     Button register;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +22,7 @@ public class RegistrationActivity extends AppCompatActivity{
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
         register = (Button) findViewById(R.id.register);
+        progressDialog = new ProgressDialog(this);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,6 +33,12 @@ public class RegistrationActivity extends AppCompatActivity{
     private void RegisterUser(){
         String passwordText = password.getText().toString().trim();
         String emailText = email.getText().toString().trim();
+        if(!(TextUtils.isEmpty(passwordText) && TextUtils.isEmpty(emailText)) ){
+
+        }
+        else {
+            Toast.makeText(this,"Username/Password is invalid",Toast.LENGTH_LONG).show();
+        }
     }
 
 }
